8b998b258fb3c418fe6cbec2c700ce83cf933694
Owen V. Hayes <owen.v.hayes@gmail.com>
2022-12-18 14:29:29 -0800

    Removed unnecessary sleep.

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ fn main() {
     let file2 = BufReader::new(File::open("data/hello.ogg").unwrap());
     let source = Decoder::new(file).unwrap();
     let source2 = Decoder::new(file2).unwrap();
-    std::thread::sleep(std::time::Duration::from_secs(5));
     let mixed = source.mix(source2);
     stream_handle.play_raw(mixed.convert_samples()).unwrap();
     std::thread::sleep(std::time::Duration::from_secs(5));
