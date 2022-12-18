1e3c8b35a7973db6c84b2b9f0c53478fb372b500
Owen V. Hayes <marvelousanything@gmail.com>
2022-12-17 22:53:56 -0800

    Added example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::fs::File;
+use std::io::BufReader;
+use rodio::{Decoder, OutputStream, Source};
+
 fn main() {
-    println!("Hello, world!");
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let file = BufReader::new(File::open("data/hello.ogg").unwrap());
+    let source = Decoder::new(file).unwrap();
+    stream_handle.play_raw(source.convert_samples()).unwrap();
+    std::thread::sleep(std::time::Duration::from_secs(5));
 }
