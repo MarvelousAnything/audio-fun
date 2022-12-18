6b8577eca51b4147e3f049e484e50f30c01cf3a3
Owen V. Hayes <owen.v.hayes@gmail.com>
2022-12-18 13:47:24 -0800

    Test mix

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
+use rodio::{Decoder, OutputStream, Source};
 use std::fs::File;
 use std::io::BufReader;
-use rodio::{Decoder, OutputStream, Source};
 
 fn main() {
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     let file = BufReader::new(File::open("data/hello.ogg").unwrap());
+    let file2 = BufReader::new(File::open("data/hello.ogg").unwrap());
     let source = Decoder::new(file).unwrap();
-    stream_handle.play_raw(source.convert_samples()).unwrap();
+    let source2 = Decoder::new(file2).unwrap();
+    std::thread::sleep(std::time::Duration::from_secs(5));
+    let mixed = source.mix(source2);
+    stream_handle.play_raw(mixed.convert_samples()).unwrap();
     std::thread::sleep(std::time::Duration::from_secs(5));
 }
